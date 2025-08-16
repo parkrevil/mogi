a5c173fbf0db4ac23c75f587ab11ef791327f346
revil <revil.com@gmail.com>
2025-08-16 15:30:56 +0000

    feat: 석션 클라이언트 데이터풀 적용

--- apps/suction-client/main.go
+++ apps/suction-client/main.go
@@ -14,6 +14,9 @@ func main() {
 		fx.Provide(NewQuicClient),
 		fx.Invoke(func(logger *zap.Logger, quicClient *QuicClient) {
 			logger.Info("Starting application")
+			
+			// Start test data generation in background
+			go quicClient.generateTestData()
 		}),
 		fx.Invoke(func(lc fx.Lifecycle, logger *zap.Logger) {
 			lc.Append(fx.Hook{
--- apps/suction-client/quic_client.go
+++ apps/suction-client/quic_client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"go-shared/common"
@@ -17,18 +19,74 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// DataPool represents a thread-safe pool for collecting sensor data
+type DataPool struct {
+	mu    sync.RWMutex
+	items []*pb.ClientData
+}
+
+// NewDataPool creates a new data pool
+func NewDataPool() *DataPool {
+	return &DataPool{
+		items: make([]*pb.ClientData, 0),
+	}
+}
+
+// AddData adds data to the pool in a thread-safe manner
+func (dp *DataPool) AddData(data *pb.ClientData) {
+	dp.mu.Lock()
+	defer dp.mu.Unlock()
+	dp.items = append(dp.items, data)
+}
+
+// GetAndClearData retrieves all data from the pool and clears it
+func (dp *DataPool) GetAndClearData() []*pb.ClientData {
+	dp.mu.Lock()
+	defer dp.mu.Unlock()
+	
+	if len(dp.items) == 0 {
+		return nil
+	}
+	
+	// Create a copy of the data
+	data := make([]*pb.ClientData, len(dp.items))
+	copy(data, dp.items)
+	
+	// Clear the pool
+	dp.items = dp.items[:0]
+	
+	return data
+}
+
+// GetDataCount returns the current number of items in the pool
+func (dp *DataPool) GetDataCount() int {
+	dp.mu.RLock()
+	defer dp.mu.RUnlock()
+	return len(dp.items)
+}
+
 type QuicClient struct {
-	logger *zap.Logger
-	config *common.Config
-	tls    *common.Tls
-	conn   *quic.Conn
+	logger   *zap.Logger
+	config   *common.Config
+	tls      *common.Tls
+	conn     *quic.Conn
+	dataPool *DataPool
+}
+
+// AddExternalData adds external data to the client's data pool
+func (qc *QuicClient) AddExternalData(data *pb.ClientData) {
+	qc.dataPool.AddData(data)
+	qc.logger.Debug("External data added to pool", 
+		zap.Int("pool_size", qc.dataPool.GetDataCount()),
+		zap.String("message", data.Message))
 }
 
 func NewQuicClient(logger *zap.Logger, config *common.Config, tls *common.Tls, lifecycle fx.Lifecycle) (*QuicClient, error) {
 	client := &QuicClient{
-		logger: logger,
-		config: config,
-		tls:    tls,
+		logger:   logger,
+		config:   config,
+		tls:      tls,
+		dataPool: NewDataPool(),
 	}
 
 	var cancel context.CancelFunc
@@ -132,24 +190,26 @@ func (qc *QuicClient) handleConnection(ctx context.Context) error {
 				zap.String("reason", ctx.Err().Error()))
 			return ctx.Err()
 		case <-ticker.C:
-			// Generate simple test data
-			sensorReadings := make([]float32, 1000) // 1000 sensor readings
-			for i := 0; i < 1000; i++ {
-				sensorReadings[i] = rand.Float32() * 100.0
-			}
+			// Get all data from the pool
+			poolData := qc.dataPool.GetAndClearData()
 			
-			// Create protobuf message with simple data
-			clientData := &pb.ClientData{
+			if len(poolData) == 0 {
+				qc.logger.Debug("No data in pool to send")
+				continue
+			}
+
+			// Create batch message with all collected data
+			batchMessage := &pb.ClientData{
 				Timestamp:      time.Now().Unix(),
-				Message:        "Test message from client",
-				SensorReadings: sensorReadings,
+				Message:        fmt.Sprintf("Batch transmission - %d items", len(poolData)),
+				SensorReadings: make([]float32, 0),
 			}
-			
-			// Marshal to protobuf
-			protobufData, err := proto.Marshal(clientData)
-			if err != nil {
-				qc.logger.Error("Failed to marshal protobuf message", zap.Error(err))
-				return err
+
+			// Combine all sensor readings from collected data
+			totalSensors := 0
+			for _, data := range poolData {
+				totalSensors += len(data.SensorReadings)
+				batchMessage.SensorReadings = append(batchMessage.SensorReadings, data.SensorReadings...)
 			}
 
 			// TODO: Snappy Compression Testing Requirements
@@ -187,18 +247,24 @@ func (qc *QuicClient) handleConnection(ctx context.Context) error {
 			//    - Implement adaptive compression based on data characteristics
 			
 			// Compress data with snappy
+			protobufData, err := proto.Marshal(batchMessage)
+			if err != nil {
+				qc.logger.Error("Failed to marshal protobuf message", zap.Error(err))
+				return err
+			}
+
 			compressedData := snappy.Encode(nil, protobufData)
 			compressionRatio := float64(len(compressedData)) / float64(len(protobufData)) * 100
 
 			_, err = stream.Write(compressedData)
 			if err != nil {
 				qc.logger.Error("Failed to write to stream", zap.Error(err))
-
 				return err
 			}
 
-			qc.logger.Info("Snappy compressed protobuf transmission", 
-				zap.Int("sensor_count", len(sensorReadings)),
+			qc.logger.Info("Batch snappy compressed protobuf transmission", 
+				zap.Int("batch_items", len(poolData)),
+				zap.Int("total_sensors", totalSensors),
 				zap.Int("protobuf_size", len(protobufData)),
 				zap.Int("compressed_size", len(compressedData)),
 				zap.Float64("compression_ratio_percent", compressionRatio),
@@ -206,4 +272,33 @@ func (qc *QuicClient) handleConnection(ctx context.Context) error {
 		}
 	}
 }
-// test comment
+
+// generateTestData simulates external data being added to the pool
+func (qc *QuicClient) generateTestData() {
+	// TODO 10ms 로 테스트시 서버에서 압축해제 에러가 발생함
+	// 100ms 로 테스트시 서버에서 압축해제 에러가 발생하지 않음
+	// 실제 데이터 적용할때 로직 개선해야됨
+	// TODO 데이터 스키마가 정해진 상태가 아니라 임시로직으로 남겨둠.
+	// 스키마가 정해지면 로직 자체가 변경되어야 함
+	ticker := time.NewTicker(100 * time.Millisecond) // Add data every 100ms
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// Generate test data
+			sensorReadings := make([]float32, 100+rand.Intn(200)) // 100-300 sensors
+			for i := 0; i < len(sensorReadings); i++ {
+				sensorReadings[i] = rand.Float32() * 100.0
+			}
+
+			testData := &pb.ClientData{
+				Timestamp:      time.Now().Unix(),
+				Message:        fmt.Sprintf("Test data %d", rand.Intn(1000)),
+				SensorReadings: sensorReadings,
+			}
+
+			qc.AddExternalData(testData)
+		}
+	}
+}
